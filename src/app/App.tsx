f222fcdd6db7fa7cfefdf4e5e79c98520b2f1616
Justin MBP M1 <justintankh@gmail.com>
2023-06-11 15:03:18 +0800

    feat: css and init logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,133 @@
 import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+	topMenuObject,
+	calculatorTopMenu,
+	calculatorTop,
+	calculatorMid,
+	calculatorBtm,
+	calculatorBtmMenu,
+} from "./const";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [amount, setAmount] = useState<number>(0);
+	const [storedAmount, setStoredAmount] = useState<number>(0);
+	const [decimalAmount, setDecimalAmount] = useState<number>(0);
+	const [decimalFlag, setDecimalFlag] = useState<boolean>(false);
+	const [compute, setCompute] =
+		useState<topMenuObject["buttonLabel"]>(undefined);
+
+	function displayAmount(amount: number, decimalAmount: number): string {
+		return `${amount}.${decimalAmount}`;
+	}
+
+	function clearDisplay(amount: number, decimalAmount: number) {
+		const numberFormat = Number(displayAmount(amount, decimalAmount));
+		setStoredAmount(numberFormat);
+		setAmount(0);
+		setDecimalAmount(0);
+	}
+
+	function addNumber(numberKey: string) {
+		console.log({ numberKey });
+		if (decimalFlag) {
+			setDecimalAmount((prevValue) => Number(`${prevValue}` + numberKey));
+		} else {
+			setAmount((prevValue) => Number(`${prevValue}` + numberKey));
+		}
+	}
+
+	function computeAmount() {
+		switch (compute) {
+			case "%":
+				setAmount(storedAmount % amount);
+				break;
+			case "-":
+				setAmount(storedAmount - amount);
+				break;
+			case "+":
+				setAmount(storedAmount + amount);
+				break;
+			case "x":
+				setAmount(storedAmount * amount);
+				break;
+			case ".":
+				setDecimalFlag((flag) => !flag);
+				setAmount(storedAmount);
+			default:
+				setAmount(storedAmount);
+				break;
+		}
+	}
+
+	function RenderMenuRow(menuConfig: topMenuObject[]) {
+		return menuConfig.map(({ buttonLabel }) => {
+			return (
+				<button
+					className="btn-top-menu"
+					key={buttonLabel}
+					onKeyDown={() => {
+						console.log("PRESSED");
+						clearDisplay(amount, decimalAmount);
+						setCompute(buttonLabel);
+					}}>
+					{buttonLabel}
+				</button>
+			);
+		});
+	}
+
+	function RenderNumberRow(list: string[]) {
+		return list.map((item) => {
+			return (
+				<button
+					className={"btn-number "}
+					key={item}
+					onKeyDown={() => {
+						addNumber(item);
+					}}>
+					{item}
+				</button>
+			);
+		});
+	}
+
+	function RenderEqualButton() {
+		return (
+			<button
+				className={"btn-number btn-equal"}
+				key={"="}
+				onKeyDown={computeAmount}>
+				{"="}
+			</button>
+		);
+	}
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
+		<div className="flexColumn calculatorModel">
+			<div className="flexColumn display">{`${displayAmount(
+				amount,
+				decimalAmount
+			)}`}</div>
+			<div className="flexRow">{RenderMenuRow(calculatorTopMenu)}</div>
+			<div className="numberRow">
+				<div className="flexColumn">
+					<div className="flexRow">
+						{RenderNumberRow(calculatorTop)}
+					</div>
+					<div className="flexRow">
+						{RenderNumberRow(calculatorMid)}
+					</div>
+					<div className="flexRow">
+						{RenderNumberRow(calculatorBtm)}
+					</div>
+					<div className="flexRow">
+						{RenderNumberRow(calculatorBtmMenu)}
+					</div>
+				</div>
+				<div>{RenderEqualButton()}</div>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</div>
 	);
 }
 
---
+++ src/app/const.ts
@@ -0,0 +1,23 @@
+export type topMenuObject = {
+	buttonLabel: "+" | "-" | "x" | "%" | "." | "AC" | undefined;
+};
+
+export const calculatorTopMenu: topMenuObject[] = [
+	{
+		buttonLabel: "+",
+	},
+	{
+		buttonLabel: "-",
+	},
+	{
+		buttonLabel: "x",
+	},
+	{
+		buttonLabel: "%",
+	},
+];
+
+export const calculatorTop = ["7", "8", "9"];
+export const calculatorMid = ["4", "5", "6"];
+export const calculatorBtm = ["1", "2", "3"];
+export const calculatorBtmMenu = ["0", ".", "AC"];
